d4a4c4d15195fcae24072a727c72c0faa07aac7b
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-08-13 13:09:38 +0600

    onUpdated Hook

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <script setup>
-import { ref, onBeforeMount } from 'vue';
-const apiResponse = ref(null)
+import { ref, onUpdated } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+const message = ref("Hello World!")
 
-onBeforeMount(() => {
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => {
-      apiResponse.value = json
-    })
+onUpdated(() => {
+  console.log("App.vue updated")
 })
-
 </script>
 
 <template>
-  <p>{{ apiResponse }}</p>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <h1>{{ message }}</h1>
+  <button @click="message='Hello from ostad'">Update Message</button>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
